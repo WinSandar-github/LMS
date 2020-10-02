5ac91122b679f3105377c5678411ed5b4eec069e
Win Sandar <sandar@agga.io>
2020-10-02 20:13:05 +0630

    updating register form design

--- Backend/app/Http/Controllers/RegisterController.php
+++ Backend/app/Http/Controllers/RegisterController.php
@@ -18,50 +18,46 @@ class RegisterController extends Controller
 {
   public function saveCompany(Request $request)
   {
-           try{
-            $string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-            $company_name=$request->input('companyname');
-            $com_len=strlen($company_name);
-            if($com_len==1){
-              $str = str_shuffle($string);
-              $ref_initials=$company_name[0].(substr($str,0,2));
-              }
-            else if($com_len==2){
-              $str = str_shuffle($string);
-              $ref_initials=$company_name[0].$company_name[1].(substr($str,0,1));
-            }
-            else{
-              $ref_initials=$company_name[0].$company_name[1].$company_name[2];
-            }
-            $company=new tbl_company();
-            $company->name=$request->input('companyname');
-            $company->address=$request->input('companyaddress');
-            $company->zipcode=$request->input('zip_code');
-            $company->phone=$request->input('txt_phone');
-            $file_ext=$request->input('ext');
-            $file_name=$ref_initials.".".$file_ext;
-            $request->file('img')->storeAs('public/company_logo',$file_name);
-            $company->logo=$file_name;
-            $company->ref_initials = strtoupper($ref_initials);
-            $company->save();
+    try{
+       $string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+       $company_name=$request->input('companyname');
+       $com_len=strlen($company_name);
+       if($com_len==1){
+         $str = str_shuffle($string);
+         $ref_initials=$company_name[0].(substr($str,0,2));
+         }
+       else if($com_len==2){
+         $str = str_shuffle($string);
+         $ref_initials=$company_name[0].$company_name[1].(substr($str,0,1));
+       }
+       else{
+         $ref_initials=$company_name[0].$company_name[1].$company_name[2];
+       }
+       $company=new tbl_company();
+       $company->name=$request->input('companyname');
+       $company->address=$request->input('companyaddress');
+       $company->phone=$request->input('txt_phone');
+       $file_ext=$request->input('ext');
+       $file_name=$ref_initials.".".$file_ext;
+       $request->file('img')->storeAs('public/company_logo',$file_name);
+       $company->logo=$file_name;
+       $company->ref_initials = strtoupper($ref_initials);
+       $company->save();
 
-            $user=new User();
-            $user->display_name=$request->input('txt_name');
-            $user->full_name=$request->input('txt_name');
-            $user->nrc_passport=$request->input('nrc_passport');
-            $user->phone_no=$request->input('txt_phone');
-            $user->address=$request->input('companyaddress');
-            $user->birthday=$request->input('birthday');
-            $user->role=$request->input('txt_type');
-            $user->company_id=$company->id;
-            $user->email=$request->input('txt_email');
-            $user->password=Hash::make($request->input('txt_password'));
-            $user->api_key=str_random(40);
-            $user->save();
-            return response()->json(config('common.successMessage'), 200,config('common.header'), JSON_UNESCAPED_UNICODE);
-          }catch (\Exception $e){
-             return response()->json(config('common.errorMessage'), 500, config('common.header'), JSON_UNESCAPED_UNICODE);
-           }
-      }
+       $user=new User();
+       $user->display_name=$request->input('txt_name');
+       $user->full_name=$request->input('txt_name');
+       $user->phone_no=$request->input('txt_phone');
+       $user->address=$request->input('companyaddress');
+       $user->company_id=$company->id;
+       $user->email=$request->input('txt_email');
+       $user->password=Hash::make($request->input('txt_password'));
+       $user->api_key=str_random(40);
+       $user->save();
+      return response()->json(config('common.successMessage'), 200,config('common.header'), JSON_UNESCAPED_UNICODE);
+   }catch (\Exception $e){
+     return response()->json(config('common.errorMessage'), 500, config('common.header'), JSON_UNESCAPED_UNICODE);
+    }
+  }
 }
  ?>
--- Frontend/Utilities/js/delivery.js
+++ Frontend/Utilities/js/delivery.js
@@ -1,110 +1,106 @@
 function saveDelivery()
 {
-        var carNumber = $("#car_number").val();
-        var driverName = $("#driver_name").val();
-        var driverPhone = $("#driver_phone").val();
-        var fromCityName = $("#from_city_name").val();
-        var toCityName = $("#to_city_name").val();
-        var startedDate = $("#start_dt").val();
-        var arrivedDate = $("#arrived_dt").val();
-        var differentArrived = $("#different_day").val();
-        var remark = $("#delivery_remark").val();
-        var status=$('#selected_status').val();
+  var carNumber = $("#car_number").val();
+  var driverName = $("#driver_name").val();
+  var driverPhone = $("#driver_phone").val();
+  var fromCityName = $("#from_city_name").val();
+  var toCityName = $("#to_city_name").val();
+  var startedDate = $("#start_dt").val();
+  var arrivedDate = $("#arrived_dt").val();
+  var differentArrived = $("#different_day").val();
+  var remark = $("#delivery_remark").val();
+  var status=$('#selected_status').val();
 
-        var delivery = new FormData;
-        delivery.append('carNumber',carNumber);
-        delivery.append('driverName',driverName);
-        delivery.append('driverPhone',driverPhone);
-        delivery.append('fromCityName',fromCityName);
-        delivery.append('toCityName',toCityName);
-        delivery.append('startedDate',startedDate);
-        delivery.append('arrivedDate',arrivedDate);
-        delivery.append('differentArrived',differentArrived);
-        delivery.append('remark',remark);
-        delivery.append('companyId',company_id);
-        delivery.append('status',status);
-        delivery.append('userId',user_id);
-        if(differentArrived.trim()==""){
-          $("#start_dt").focus();
-          }
-        else if(differentArrived==-1){
-           $("#arrived_dt").focus();
-        }
-        else{
-          $.ajax({
-                  type: "POST",
-                  url: BACKEND_URL + "saveDelivery",
-                  data: delivery,
-                  contentType: false,
-                  processData: false,
-                  success: function (data) {
-                    alert(data.message);
-                    $('#modal-delivery').modal('toggle');
-                    destroyDatatable("#table_tbl_delivery","#tbl_delivery_container");
-                    loadDelivery();
-                 },
-                 error:function (XMLHttpRequest, textStatus, errorThrown){
-                   errorStatus(XMLHttpRequest, textStatus, errorThrown);
-                 }
-
-          });
-        }
-
+  var delivery = new FormData;
+  delivery.append('carNumber',carNumber);
+  delivery.append('driverName',driverName);
+  delivery.append('driverPhone',driverPhone);
+  delivery.append('fromCityName',fromCityName);
+  delivery.append('toCityName',toCityName);
+  delivery.append('startedDate',startedDate);
+  delivery.append('arrivedDate',arrivedDate);
+  delivery.append('differentArrived',differentArrived);
+  delivery.append('remark',remark);
+  delivery.append('companyId',company_id);
+  delivery.append('status',status);
+  delivery.append('userId',user_id);
+  if(differentArrived.trim()==""){
+    $("#start_dt").focus();
+    }
+  else if(differentArrived==-1){
+     $("#arrived_dt").focus();
+  }
+  else{
+    $.ajax({
+            type: "POST",
+            url: BACKEND_URL + "saveDelivery",
+            data: delivery,
+            contentType: false,
+            processData: false,
+            success: function (data) {
+              alert(data.message);
+              $('#modal-delivery').modal('toggle');
+              destroyDatatable("#table_tbl_delivery","#tbl_delivery_container");
+              loadDelivery();
+           },
+           error:function (XMLHttpRequest, textStatus, errorThrown){
+             errorStatus(XMLHttpRequest, textStatus, errorThrown);
+           }
+         });
+  }
 }
-function getDifferentDays() {
+function getDifferentDays()
+{
+  var startDate = $("#start_dt").val();
+  var endDate = $("#arrived_dt").val();
 
-        var startDate = $("#start_dt").val();
-        var endDate = $("#arrived_dt").val();
-
-        if (endDate.trim() == "") {
-            $("#different_day").val("-1");
-          }
-        else {
-            var firstDate = new Date(startDate);
-            var lastDate = new Date(endDate);
-            const differentTime = Math.abs(firstDate.getTime() - lastDate.getTime());
-            const differentDay = Math.ceil(differentTime / (1000 * 60 * 60 * 24));
-            $("#different_day").val(differentDay);
-        }
-
-
+  if (endDate.trim() == "") {
+      $("#different_day").val("-1");
+    }
+  else {
+      var firstDate = new Date(startDate);
+      var lastDate = new Date(endDate);
+      const differentTime = Math.abs(firstDate.getTime() - lastDate.getTime());
+      const differentDay = Math.ceil(differentTime / (1000 * 60 * 60 * 24));
+      $("#different_day").val(differentDay);
+  }
 }
 
 function loadDelivery()
 {
-        destroyDatatable("#table_tbl_delivery","#tbl_delivery_container");
-        $.ajax({
-                type: "POST",
-                url: BACKEND_URL + "getDelivery",
-                data: "companyId=" + company_id,
-                success: function (data) {
-                  console.log(data);
-                  data.forEach(function (element) {
-                    var tr = "<tr onclick='getDeliverDetailsByDeliveryId(" + element.id + ");'>";
-                    tr += "<td >" + element.car_no + "</td>";
-                    tr += "<td >" + element.driver_name + "</td>";
-                    tr += "<td >" + element.driver_phone + "</td>";
-                    tr += "<td >" + element.depart_from + "</td>";
-                    tr += "<td >" + element.depart_to + "</td>";
-                    tr += "<td >" + element.start_dt + "</td>";
-                    tr += "<td >" + element.end_dt + "</td>";
-                    tr += "<td >" + element.arrived + "</td>";
-                    tr += "<td >" + element.remark + "</td>";
-                    tr += "<td >" + element.users['full_name'] + "</td>";
-                    tr += "<td >" + element.company_delivery['name'] + "</td>";
-                    tr += "<td class='alignright'><button type='button' class='btn btn-product' data-toggle='modal' data-target='#modal-delivery_details' onClick=getDeliveryById("+ element.id +")><i class='fas fa-plus'></i>  Add DeliveryDetails</button ></td >" ;
-                    tr += "<td class='alignright'><button type='button' class='btn btn-edit' onClick=showDeliveryById("+ element.id +")><i class='fas fa-edit'></i></button ></td > ";
-                    tr += "<td class='alignright'><button type='button' class='btn btn-print' onClick=printDeliveryById("+ element.id +")><i class='fas fa-print'> Print</button ></td > ";
-                    tr += "</tr>";
-                    $("#tbl_delivery_container").append(tr);
+  destroyDatatable("#table_tbl_delivery","#tbl_delivery_container");
+  $.ajax({
+          type: "POST",
+          url: BACKEND_URL + "getDelivery",
+          data: "companyId=" + company_id,
+          success: function (data) {
+            console.log(data);
+            data.forEach(function (element) {
+              var tr = "<tr onclick='getDeliverDetailsByDeliveryId(" + element.id + ");'>";
+              tr += "<td >" + element.car_no + "</td>";
+              tr += "<td >" + element.driver_name + "</td>";
+              tr += "<td >" + element.driver_phone + "</td>";
+              tr += "<td >" + element.depart_from + "</td>";
+              tr += "<td >" + element.depart_to + "</td>";
+              tr += "<td >" + element.start_dt + "</td>";
+              tr += "<td >" + element.end_dt + "</td>";
+              tr += "<td >" + element.arrived + "</td>";
+              tr += "<td >" + element.remark + "</td>";
+              tr += "<td >" + element.users['full_name'] + "</td>";
+              tr += "<td >" + element.company_delivery['name'] + "</td>";
+              tr += "<td class='alignright'><button type='button' class='btn btn-product' data-toggle='modal' data-target='#modal-delivery_details' onClick=getDeliveryById("+ element.id +")><i class='fas fa-plus'></i>  Add DeliveryDetails</button ></td >" ;
+              tr += "<td class='alignright'><button type='button' class='btn btn-edit' onClick=showDeliveryById("+ element.id +")><i class='fas fa-edit'></i></button ></td > ";
+              tr += "<td class='alignright'><button type='button' class='btn btn-print' onClick=printDeliveryById("+ element.id +")><i class='fas fa-print'> Print</button ></td > ";
+              tr += "</tr>";
+              $("#tbl_delivery_container").append(tr);
 
-                });
-                createDataTable('#table_tbl_delivery');
-                },
-                error:function (message){
-                  errorMessage(message);
-                }
-        });
+          });
+          createDataTable('#table_tbl_delivery');
+          },
+          error:function (message){
+            errorMessage(message);
+          }
+  });
 }
 function getDeliveryById(deliveryId)
 {
@@ -112,66 +108,66 @@ function getDeliveryById(deliveryId)
 }
 function saveDeliverDetail()
 {
-        var tableBody = document.getElementById("product_table");   //tbl_order_details
-        var deliveryId=$('#hdeliveryId').val();
-        var goodReceiptId=1;                                  //tbl_good_receipt
-        var goodReceiptDetailId=1;                           //tbl_good_receipt_details
-        var orderDetailsId=1;                                 //tbl_order_details
-        var receiverName='U Tun';                              //row.cells[1].innerHTML
-        var productName='test';                                //row.cells[2].innerHTML
-        var totalQuantity='100';                               //row.cells[3].innerHTML
-        var quantity='10';                                      //row.cells[4].innerHTML
-        var weight='10kg';                                      //row.cells[5].innerHTML
-        var toCityName='Monywa';                              //row.cells[6].innerHTML
-        var remark='description';                               //row.cells[7].innerHTML
-        var deliveryDetails = new FormData;
-        for (var i = 0, row; row = tableBody.rows[i]; i++)//tbl_order_details
-        {
-            var productData={};
-            productData['receiverName']=row.cells[1].innerHTML;   //tbl_order_details
-            productData['productName']=row.cells[2].innerHTML;    //tbl_order_details
-            productData['totalQuantity']=row.cells[3].innerHTML;  //tbl_order_details
-            productData['quantity']=row.cells[4].innerHTML;       //tbl_order_details
-            productData['weight']=row.cells[5].innerHTML;         //tbl_order_details
-            productData['toCityName']=row.cells[6].innerHTML;     //tbl_order_details
-            productData['remark']=row.cells[7].innerHTML;         //tbl_order_details
-            productData['userId']=user_id;
-            productData['companyId']=company_id;
-            productData['deliveryId']=deliveryId;
-            productData['goodReceiptId']=goodReceiptId;
-            productData['goodReceiptDetailId']=goodReceiptDetailId;
-            productData['order_details_id']=orderDetailsId;
+  var tableBody = document.getElementById("product_table");   //tbl_order_details
+  var deliveryId=$('#hdeliveryId').val();
+  var goodReceiptId=1;                                  //tbl_good_receipt
+  var goodReceiptDetailId=1;                           //tbl_good_receipt_details
+  var orderDetailsId=1;                                 //tbl_order_details
+  var receiverName='U Tun';                              //row.cells[1].innerHTML
+  var productName='test';                                //row.cells[2].innerHTML
+  var totalQuantity='100';                               //row.cells[3].innerHTML
+  var quantity='10';                                      //row.cells[4].innerHTML
+  var weight='10kg';                                      //row.cells[5].innerHTML
+  var toCityName='Monywa';                              //row.cells[6].innerHTML
+  var remark='description';                               //row.cells[7].innerHTML
+  var deliveryDetails = new FormData;
+  for (var i = 0, row; row = tableBody.rows[i]; i++)//tbl_order_details
+  {
+      var productData={};
+      productData['receiverName']=row.cells[1].innerHTML;   //tbl_order_details
+      productData['productName']=row.cells[2].innerHTML;    //tbl_order_details
+      productData['totalQuantity']=row.cells[3].innerHTML;  //tbl_order_details
+      productData['quantity']=row.cells[4].innerHTML;       //tbl_order_details
+      productData['weight']=row.cells[5].innerHTML;         //tbl_order_details
+      productData['toCityName']=row.cells[6].innerHTML;     //tbl_order_details
+      productData['remark']=row.cells[7].innerHTML;         //tbl_order_details
+      productData['userId']=user_id;
+      productData['companyId']=company_id;
+      productData['deliveryId']=deliveryId;
+      productData['goodReceiptId']=goodReceiptId;
+      productData['goodReceiptDetailId']=goodReceiptDetailId;
+      productData['order_details_id']=orderDetailsId;
 
-        }
+  }
 
-        deliveryDetails.append('receiverName',receiverName);
-        deliveryDetails.append('productName',productName);
-        deliveryDetails.append('totalQuantity',totalQuantity);
-        deliveryDetails.append('quantity',quantity);
-        deliveryDetails.append('weight',weight);
-        deliveryDetails.append('toCityName',toCityName);
-        deliveryDetails.append('remark',remark);
-        deliveryDetails.append('userId',user_id);
-        deliveryDetails.append('companyId',company_id);
-        deliveryDetails.append('deliveryId',deliveryId);
-        deliveryDetails.append('goodReceiptId',goodReceiptId);
-        deliveryDetails.append('goodReceiptDetailId',goodReceiptDetailId);
-        deliveryDetails.append('orderDetailsId',orderDetailsId);
+  deliveryDetails.append('receiverName',receiverName);
+  deliveryDetails.append('productName',productName);
+  deliveryDetails.append('totalQuantity',totalQuantity);
+  deliveryDetails.append('quantity',quantity);
+  deliveryDetails.append('weight',weight);
+  deliveryDetails.append('toCityName',toCityName);
+  deliveryDetails.append('remark',remark);
+  deliveryDetails.append('userId',user_id);
+  deliveryDetails.append('companyId',company_id);
+  deliveryDetails.append('deliveryId',deliveryId);
+  deliveryDetails.append('goodReceiptId',goodReceiptId);
+  deliveryDetails.append('goodReceiptDetailId',goodReceiptDetailId);
+  deliveryDetails.append('orderDetailsId',orderDetailsId);
 
-        $.ajax({
-                type: "POST",
-                url: BACKEND_URL + "saveDeliverDetail",
-                data: deliveryDetails,
-                contentType: false,
-                processData: false,
-                success: function (data) {
-                  alert(data.message);
-                  $('#modal-delivery_details').modal('toggle');
-               },
-               error:function (XMLHttpRequest, textStatus, errorThrown){
-                 errorStatus(XMLHttpRequest, textStatus, errorThrown);
-               }
-            });
+  $.ajax({
+          type: "POST",
+          url: BACKEND_URL + "saveDeliverDetail",
+          data: deliveryDetails,
+          contentType: false,
+          processData: false,
+          success: function (data) {
+            alert(data.message);
+            $('#modal-delivery_details').modal('toggle');
+         },
+         error:function (XMLHttpRequest, textStatus, errorThrown){
+           errorStatus(XMLHttpRequest, textStatus, errorThrown);
+         }
+      });
 
 }
 function getDeliverDetailsByDeliveryId(deliveryId)
@@ -516,8 +512,8 @@ function autoOrderNo()
           }
       });
 }
-var count=0;
-var sumQty=0;
+const count=0;
+const sumQty=0;
 function searchOrderNo(orderNo)
 {
   $.ajax({
